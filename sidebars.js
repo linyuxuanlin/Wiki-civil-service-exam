09a2693d1528962a8c854af0a18b64f3001e458f
linyuxuanlin <824676271@qq.com>
2021-01-18 12:44:35 +0800

    up

--- sidebars.js
+++ sidebars.js
@@ -10,7 +10,17 @@ module.exports = {
           type: 'link',
           label: '公务员 & 事业单位结构化面试经验分享（详细）', 
           href: 'https://www.douban.com/note/785769898/'
-        }
+        },
+        {
+          type: 'link',
+          label: 'pushmetop/civil-service-exam', 
+          href: 'https://github.com/pushmetop/civil-service-exam'
+        },
+        {
+          type: 'link',
+          label: 'kuriv/civil-service-exam', 
+          href: 'https://github.com/kuriv/civil-service-exam'
+        },
       ],
     },
 
